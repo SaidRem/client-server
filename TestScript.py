be96d8830b0e5afec6c822cbdcd39d3a8a53331c
Said Remiev <59606092+SaidRem@users.noreply.github.com>
2020-05-26 21:25:39 +0400

    Update TestScript.py

--- TestScript.py
+++ TestScript.py
@@ -31,23 +31,23 @@ def run(host, port):
         sys.exit(1)
 
     assert data_1 == data_2 == {}, \
-        "На запрос клиента на получения данных по не существующему ключу, сервер " \
-        "вдолжен озвращать ответ с пустым полем данных."
+        "When a client requests to receive data on a non existing key, the server" \
+        "must return a response with an empty data field"
 
     try:
         data_1 = client1.get(command)
         data_2 = client2.get(command)
     except ClientError:
-        print('Сервер вернул ответ на валидный запрос, который клиент определил'
-              ', как не корректный.. ')
+        print('The server reterned a response to a valid request, which'
+              ' client identified as incorrect')
     except BaseException as err:
-        print(f"Сервер должен поддерживать соединение с несколькими клиентами: "
+        print(f"The server must support connection with several clients: "
               f"{err.__class__}: {err}")
         sys.exit(1)
 
     assert data_1 == data_2 == {}, \
-        "На запрос клиента на получения данных по не существующему ключу, сервер " \
-        "должен возвращать ответ с пустым полем данных."
+        "When a client requests to receive data on a non existing key, the server " \
+        "must return a response with an empty data field"
 
     try:
         client1.put("k1", 0.25, timestamp=1)
@@ -57,7 +57,7 @@ def run(host, port):
         client1.put("k2", 40, timestamp=5)
         client1.put("k2", 41, timestamp=5)
     except Exception as err:
-        print(f"Ошибка вызова client.put(...) {err.__class__}: {err}")
+        print(f"Call error client.put(...) {err.__class__}: {err}")
         sys.exit(1)
 
     expected_metrics = {
@@ -68,11 +68,11 @@ def run(host, port):
     try:
         metrics = client1.get("*")
         if metrics != expected_metrics:
-            print(f"client.get('*') вернул неверный результат. Ожидается: "
-                  f"{expected_metrics}. Получено: {metrics}")
+            print(f"client.get('*') returned incorrect result. Expected: "
+                  f"{expected_metrics}. Received: {metrics}")
             sys.exit(1)
     except Exception as err:
-        print(f"Ошибка вызова client.get('*') {err.__class__}: {err}")
+        print(f"Call error client.get('*') {err.__class__}: {err}")
         sys.exit(1)
 
     expected_metrics = {"k2": [(4, 30.0), (5, 41.0)]}
@@ -80,26 +80,26 @@ def run(host, port):
     try:
         metrics = client2.get("k2")
         if metrics != expected_metrics:
-            print(f"client.get('k2') вернул неверный результат. Ожидается: "
-                  f"{expected_metrics}. Получено: {metrics}")
+            print(f"client.get('k2') returned incorrect result. Expected: "
+                  f"{expected_metrics}. Received: {metrics}")
             sys.exit(1)
     except Exception as err:
-        print(f"Ошибка вызова client.get('k2') {err.__class__}: {err}")
+        print(f"Call error client.get('k2') {err.__class__}: {err}")
         sys.exit(1)
 
     try:
         result = client1.get("k3")
         if result != {}:
             print(
-                f"Ошибка вызова метода get с ключом, который еще не был добавлен. "
-                f"Ожидается: пустой словарь. Получено: {result}")
+                f"Error calling get method with a key that has not been added yet. "
+                f"Expected: empty dictionare. Received: {result}")
             sys.exit(1)
     except Exception as err:
-        print(f"Ошибка вызова метода get с ключом, который еще не был добавлен: "
+        print(f"Error calling get method with a key that has not been added yet: "
               f"{err.__class__} {err}")
         sys.exit(1)
 
-    print("Похоже, что все верно! Попробуйте отправить решение на проверку.")
+    print("Everything alright.")
 
 
 if __name__ == "__main__":
